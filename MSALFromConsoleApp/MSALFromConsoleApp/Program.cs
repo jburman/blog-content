c1afecfd8154c6d02940d847de0959dc22b727ff
jburman <jburman@gmail.com>
2018-08-05 15:02:42 -0400

    Added sample for MSAL authentication from a console app (Full Framework)

---
+++ MSALFromConsoleApp/MSALFromConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Linq;
+
+namespace MSALFromConsoleApp
+{
+    // Some example code for authenticating for Graph API access via MSAL
+    // Uses local cache so that multiple runs do not need to pop authentication dialog.
+    // Uses MSAL nuget package: install-package Microsoft.Identity.Client -IncludePrerelease
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientId = "CLIENT ID HERE";
+
+            // Example scopes
+            string[] scopes = new[] { "https://graph.microsoft.com/user.read https://graph.microsoft.com/files.read" };
+
+            PublicClientApplication myApp = new PublicClientApplication(clientId, "https://login.microsoftonline.com/common", TokenCacheHelper.GetUserCache());
+            var user = myApp.Users.FirstOrDefault();
+            AuthenticationResult auth = null;
+            string token = null;
+
+            if (user != null)
+            {
+                Console.WriteLine("Getting token from cache: " + user.Name);
+                auth = myApp.AcquireTokenSilentAsync(scopes, user).GetAwaiter().GetResult();
+                if (auth != null && !string.IsNullOrEmpty(auth.IdToken))
+                    token = auth.IdToken;
+            }
+            else
+                Console.WriteLine("No token found in cache");
+
+            if (token == null)
+            {
+                auth = myApp.AcquireTokenAsync(scopes).GetAwaiter().GetResult();
+                token = auth.IdToken;
+            }
+
+            Console.WriteLine("Authenticated: " + auth.User?.Name);
+        }
+    }
+}
---
+++ MSALFromConsoleApp/MSALFromConsoleApp/TokenCacheHelper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Identity.Client;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace MSALFromConsoleApp
+{
+    // Copied from MSAL WPF sample
+    // https://github.com/Azure-Samples/active-directory-dotnet-desktop-msgraph-v2/blob/d728f5e72a778910655a114a28451c83f4ad1d19/active-directory-wpf-msgraph-v2/TokenCacheHelper.cs
+    static class TokenCacheHelper
+    {
+        /// <summary>
+        /// Get the user token cache
+        /// </summary>
+        /// <returns></returns>
+        public static TokenCache GetUserCache()
+        {
+            if (usertokenCache == null)
+            {
+                usertokenCache = new TokenCache();
+                usertokenCache.SetBeforeAccess(BeforeAccessNotification);
+                usertokenCache.SetAfterAccess(AfterAccessNotification);
+            }
+            return usertokenCache;
+        }
+
+        static TokenCache usertokenCache;
+
+        /// <summary>
+        /// Path to the token cache
+        /// </summary>
+        public static readonly string CacheFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalcache.bin";
+
+        private static readonly object FileLock = new object();
+
+        public static void BeforeAccessNotification(TokenCacheNotificationArgs args)
+        {
+            lock (FileLock)
+            {
+                args.TokenCache.Deserialize(File.Exists(CacheFilePath)
+                    ? ProtectedData.Unprotect(File.ReadAllBytes(CacheFilePath),
+                                              null,
+                                              DataProtectionScope.CurrentUser)
+                    : null);
+            }
+        }
+
+        public static void AfterAccessNotification(TokenCacheNotificationArgs args)
+        {
+            // if the access operation resulted in a cache update
+            if (args.TokenCache.HasStateChanged)
+            {
+                lock (FileLock)
+                {
+                    // reflect changesgs in the persistent store
+                    File.WriteAllBytes(CacheFilePath,
+                                       ProtectedData.Protect(args.TokenCache.Serialize(),
+                                                             null,
+                                                             DataProtectionScope.CurrentUser)
+                                      );
+                    // once the write operationtakes place restore the HasStateChanged bit to filse
+                    args.TokenCache.HasStateChanged = false;
+                }
+            }
+        }
+    }
+}
