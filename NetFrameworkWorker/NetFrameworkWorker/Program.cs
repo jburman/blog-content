7e699812e01eb945f22e2e8a54ac0e6c0df64812
Jeremy Burman <jburman@gmail.com>
2020-01-23 15:59:00 -0500

    Net Framework Worker sample

---
+++ NetFrameworkWorker/NetFrameworkWorker/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NetFrameworkWorker
+{
+    class Program
+    {
+        static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configBuilder => configBuilder.AddJsonFile("appsettings.json"))
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ NetFrameworkWorker/NetFrameworkWorker/Worker.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetFrameworkWorker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly int _timeout;
+
+        public Worker(ILogger<Worker> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _timeout = config.GetValue("WorkerTimeout", 5000);
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Executing Worker at {time}", DateTimeOffset.Now);
+
+                await Task.Delay(_timeout, stoppingToken);
+            }
+        }
+    }
+}
